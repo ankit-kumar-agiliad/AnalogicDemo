5dc825b017b901afb02caa3c8b59938f2e184d3e
ankit-kumar-agiliad <ankit.kumar@agiliad.com>
2024-11-05 12:02:24 +0530

    added changes

--- cenbackend.js
+++ cenbackend.js
@@ -1,18 +1,18 @@
 // Import required modules
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const multer = require('multer');
 const mqtt = require('mqtt');
-const multer = require("multer");
-// const { Readable } = require("stream");
-const util = require("util");
-// const fs = require("fs");
+const { Readable } = require('stream');
 
-const upload = multer({ storage: multer.memoryStorage() });
 
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+const upload = multer({ storage: multer.memoryStorage() });
+const CHUNK_SIZE = 50 * 1024 * 1024;
 
 //MQTT broker setup
 const brokerUrl = "mqtt://broker.hivemq.com:1883"
@@ -31,89 +31,77 @@ mqttClient.on('connect', () => {
     });
 })
 
-const publishMessage = util.promisify(mqttClient.publish).bind(mqttClient);
-
-mqttClient.on('message', (topic, message) => {
-    try {
-        if (topic.includes("Tip")) {
-            console.log(message.toString());
-        }
-
-    } catch (error) {
-        console.error(error)
-    }
-
-});
+function publishMessage(topic, chunk, options) {
+    return new Promise((resolve, reject) => {
+        mqttClient.publish(topic, chunk, options, (err) => {
+            if (err) {
+                console.log(`Error in publishing on topic : ${topic}`, err.message);
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    })
+}
 
 app.post('/tipUpload', upload.single('file'), async (req, res) => {
     if (!req.file) {
         return res.status(400).send("No file uploaded!");
     }
-    const chunkSize = 1024 * 1024 * 25;
     const fileBuffer = req.file.buffer;
-    const totalChunks = Math.ceil(fileBuffer.length / chunkSize);
+    const totalChunks = Math.ceil(fileBuffer.length / CHUNK_SIZE);
     const { id } = req.body;
     const topic = `TIP/nodes/${id}`;
+    let currentChunk = 0;
+    const publishPromises = [];
 
-    const promises = [];
-
-    try {
-        for (let currentChunk = 1; currentChunk <= totalChunks; currentChunk++) {
-            const start = currentChunk * chunkSize;
-            const end = Math.min(start + chunkSize, fileBuffer.length);
-            const chunk = fileBuffer.slice(start, end);
-            const publishMessagePromise = publishMessage(topic, chunk, { qos: 1 }).then(() => {
-                console.log(`Published chunk ${currentChunk} of ${totalChunks} `)
-            }).catch(err => {
-                console.log(`Error in publishing on topic : ${topic}`, err);
-            });
-
-            promises.push(publishMessagePromise)
+    const stream = new Readable({
+        read() {
+            if (currentChunk >= totalChunks) {
+                this.push(null);
+            } else {
+                const start = currentChunk * CHUNK_SIZE;
+                const end = Math.min(start + CHUNK_SIZE, fileBuffer.length);
+                const chunk = fileBuffer.slice(start, end);
+                this.push(chunk);
+                currentChunk++;
+            }
         }
-        await Promise.all(promises);
-        res.send('File uploaded and published successfully')
+    });
 
-    } catch (error) {
-        console.error(error.message);
-    }
+    stream.on('data', async (chunk) => {
 
-    // const stream = new Readable({
-    //     read() {
-    //         const start = currentChunk * chunkSize;
-    //         const end = Math.min(start + chunkSize, fileBuffer.length);
-    //         if (start >= fileBuffer.length) {
-    //             this.push(null);
-    //         } else {
-    //             this.push(fileBuffer.slice(start, end));
-    //             currentChunk += 1;
-    //         }
-    //     }
-    // })
+        const publishPromise = publishMessage(topic, chunk, { qos: 1 })
+            .then(() => {
+                console.log(`Published chunk ${currentChunk} of ${totalChunks} `);
+            }).catch(err => console.error(err))
 
-    // stream.on('data', (chunk) => {
-    //     console.log(chunk);
-    //     mqttClient.publish(topic, chunk, { qos: 1, retain: true }, (err) => {
-    //         if (err) {
-    //             console.log(`Error in publishing on topic : ${topic}`, err.message);
-    //         } else {
-    //             console.log(`Published chunk ${currentChunk} of ${totalChunks} `);
-    //         }
-    //     });
-    // });
+
+        publishPromises.push(publishPromise);
+
+        // mqttClient.publish(topic, chunk, { qos: 1, retain: true }, (err) => {
+        //     if (err) {
+        //         console.log(`Error in publishing on topic : ${topic}`, err.message);
+        //     } else {
+        //         console.log(`Published chunk ${currentChunk} of ${totalChunks} `);
+        //     }
+        // });
+    });
 
     // for await (const chunk of stream) {
     //     await publishMessage(topic, chunk, { qos: 1 })
     //     console.log(`Published chunk ${currentChunk} of ${totalChunks} `);
     // }
 
-    // stream.on('end', () => {
-    //     res.send('File uploaded and published successfully')
-    // });
+    stream.on('end', async () => {
+        await Promise.all(publishPromises)
+        res.send('File uploaded and published successfully')
+    });
 
-    // stream.on('error', (err) => {
-    //     console.error("Stream error: ", err);
-    //     res.status(500).send("Failed to process file upload")
-    // });
+    stream.on('error', (err) => {
+        console.error("Stream error: ", err);
+        res.status(500).send("Failed to process file upload")
+    });
 
 
 
